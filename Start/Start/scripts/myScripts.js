b10be19bd0e0929d8559de7b25975e62de9ff2db
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-07-10 13:00:28 +0100

    session 4

--- Start/Start/scripts/myScripts.js
+++ Start/Start/scripts/myScripts.js
@@ -66,12 +66,73 @@ function summary() {
     summaryReport.innerHTML = bookings.length + " bookings made!";
 }
 
+
+/*
+        Q3. - Detailed Report
+    */
 function detailedReport() {
     let DomesticReport = document.getElementById('DomesticReport');
     let EUReport = document.getElementById('EUReport');
     let TransatlanticReport = document.getElementById('TransatlanticReport');
 
+
+    // Keeps track of how many flights were booked for each type
+    let domesticCount = 0;
+    let euCount = 0;
+    let transatlanticCount = 0;
+
+    // Keeps track of overall price for each flight type
+    let totalDomesticPrice = 0;
+    let totalEUPrice = 0;
+    let totalTransatlanticPrice = 0;
+
+
+    // Empty String to add all customers info to, to use later
+    let domesticBookings = "";
+    let euBookings = "";
+    let translanticBookings = "";
+
     for (booking in bookings) {
-        console.log(bookings[booking].custNumber);
+        if (bookings[booking].flightType === "Domestic") {
+            domesticCount++;
+            totalDomesticPrice += parseInt(bookings[booking].amount)
+            domesticBookings += `<p>Customer Number: ${bookings[booking].custNumber}</p>`
+            domesticBookings += `<p>Weight: ${bookings[booking].weight}</p>`
+            domesticBookings += `<p>Amount: ${bookings[booking].amount}</p>`
+
+        } else if (bookings[booking].flightType === "EU") {
+            euCount++;
+            totalEUPrice += parseInt(bookings[booking].amount)
+            euBookings += `<p>Customer Number: ${bookings[booking].custNumber}</p>`
+            euBookings += `<p>Weight: ${bookings[booking].weight}</p>`
+            euBookings += `<p>Amount: ${bookings[booking].amount}</p>`
+
+        } else {
+            transatlanticCount++;
+            totalTransatlanticPrice += parseInt(bookings[booking].amount)
+            translanticBookings += `<p>Customer Number: ${bookings[booking].custNumber}</p>`
+            translanticBookings += `<p>Weight: ${bookings[booking].weight}</p>`
+            translanticBookings += `<p>Amount: ${bookings[booking].amount}</p>`
+
+        }
     }
+
+    DomesticReport.innerHTML = `
+    <h4>Domestic Flight: ${domesticCount} </h4>
+    ${domesticBookings}
+    <p>Total amount paid: ${totalDomesticPrice}</p>
+    `;
+
+    EUReport.innerHTML = `
+    <h4>EU Flight: ${euCount} </h4>
+    ${euBookings}
+    <p>Total amount paid: ${totalEUPrice}</p>
+    `;
+
+    TransatlanticReport.innerHTML = `
+    <h4>Translantic Flight: ${transatlanticCount} </h4>
+    ${translanticBookings}
+    <p>Total amount paid: ${totalTransatlanticPrice}</p>
+    `;
+
 }
